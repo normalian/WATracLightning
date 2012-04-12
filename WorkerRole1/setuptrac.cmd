24a1e9fb5664677764ac1a3fa60219fd59de7c95
normalian <warito_hutsu@hotmail.com>
2012-04-13 01:42:45 +0900

    modify setup script

--- WorkerRole1/setuptrac.cmd
+++ WorkerRole1/setuptrac.cmd
@@ -2,14 +2,12 @@
 
 @rem Trac Lightning がインストールされていない場合はインストールし、再起動される
 if exist Z:\TracLight\start.bat goto runtrac
-"%~dp0Assets\download.vbs" https://normalianfeasibility.blob.core.windows.net/trac/TracLightning.exe
+"%~dp0Assets\download.vbs" https://[YOUR_STORAGE_ACCOUNT_NAME].blob.core.windows.net/trac/TracLightning.exe
 "%~dp0TracLightning.exe" /VERYSILENT /LOG="log.txt" /DIR="Z:\TracLight"
 goto end
 
-@rem Trac Lightning がインストール済みの場合、Trac Lightningを実行する
 :runtrac
-
-@rem start Z:\TracLight\start.bat
+@rem Trac Lightning がインストール済みの場合、Trac Lightningを実行する
 call Z:\TracLight\start.bat
 
 :end
